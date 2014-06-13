ee32a1cae85d2b7ddeca60931b359039ba56b65c
johnreed81 <johnreed81@gmail.com>
2014-06-13 01:02:31 -0500

    first check in to git repo

---
+++ test2/src/test2/pkg/Main.java
@@ -0,0 +1,11 @@
+package test2.pkg;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
---
+++ test2/src/test2/pkg/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author john
+ *  testing, so bear w me lol
+ */
+package test2.pkg;
